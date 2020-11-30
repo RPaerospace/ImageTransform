3bbd35c5f3d45515dcb4b04f01dcd26a83374e7b
Pablo Ruiz Royo <71186167+RPaerospace@users.noreply.github.com>
2020-11-30 11:29:18 +0100

    Initial upload

--- uiuc/HSLAPixel.h
+++ uiuc/HSLAPixel.h
@@ -1,8 +1,5 @@
 /**
  * @file HSLAPixel.h
- *
- * @author University of Illinois CS 225 Course Staff
- * @version 2018r1-lab1 - Updated for CS 400
  */
 
 #pragma once
@@ -11,9 +8,6 @@
 #include <sstream>
 
 namespace uiuc {
-
-  // Put your HSLAPixel class definition here.
-  // (Remember to end it with a semicolon!)
 
     class HSLAPixel{
         public:
